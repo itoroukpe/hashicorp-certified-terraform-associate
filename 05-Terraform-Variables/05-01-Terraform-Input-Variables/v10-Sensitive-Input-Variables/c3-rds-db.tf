5db240ec412622de188060b53a065421ecf43823
itoro Ukpe <122684000+itoroukpe@users.noreply.github.com>
2024-04-26 11:24:29 -0700

    Update c3-rds-db.tf

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v10-Sensitive-Input-Variables/c3-rds-db.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v10-Sensitive-Input-Variables/c3-rds-db.tf
@@ -3,7 +3,7 @@
 resource "aws_db_instance" "db1" {
   allocated_storage   = 5
   engine              = "mysql"
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.2xlarge"
   name                = "mydb1"
   username            = var.db_username
   password            = var.db_password
